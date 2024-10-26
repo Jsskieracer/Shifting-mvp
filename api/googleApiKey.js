8ddae8124545e431b5c40f391892bc75cb7fbbca
Jsskieracer <jasonssoll@gmail.com>
2024-10-26 06:29:25 -0700

    Update googleApiKey.js

--- api/googleApiKey.js
+++ api/googleApiKey.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+    console.log("Google API key request received.");
+
     if (process.env.GOOGLE_API_KEY) {
         console.log("Google API key found and returned.");
         res.status(200).json({ googleApiKey: process.env.GOOGLE_API_KEY });
