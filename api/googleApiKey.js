729476270f18dbcc0ad1d5af157cf3861336f2be
Jsskieracer <jasonssoll@gmail.com>
2024-10-26 06:54:19 -0700

    Update googleApiKey.js

--- api/googleApiKey.js
+++ api/googleApiKey.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
     // Check for the Google API key in environment variables
     if (process.env.GOOGLE_API_KEY) {
-        console.log("Google API key found and returned.");
+        console.log("Google API key found and sent."); // No longer exposing the actual key in logs
         res.status(200).json({ googleApiKey: process.env.GOOGLE_API_KEY });
     } else {
         console.error("Google API key not found in environment variables.");
